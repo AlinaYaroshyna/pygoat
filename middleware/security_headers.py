be15e608278b5ad81bcb006cea770e07bba7a7fd
AlinaYaroshyna <60234250+AlinaYaroshyna@users.noreply.github.com>
2025-06-01 13:11:08 +0200

    Update security_headers.py

--- middleware/security_headers.py
+++ middleware/security_headers.py
@@ -1,10 +1,10 @@
-def security_headers(get_response):
-    def middleware(request):
-        response = get_response(request)
+from django.utils.deprecation import MiddlewareMixin
+
+class SecurityHeadersMiddleware(MiddlewareMixin):
+    def process_response(self, request, response):
         response['X-Content-Type-Options'] = 'nosniff'
         response['X-Frame-Options'] = 'DENY'
         response['X-XSS-Protection'] = '1; mode=block'
         response['Content-Security-Policy'] = "default-src 'self'"
         response['Referrer-Policy'] = 'no-referrer'
         return response
-    return middleware
